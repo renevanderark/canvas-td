ec905c9f27c0f261f986d19b43fa338d731acf81
renevanderark <r.van.der.ark@gmail.com>
2013-06-21 16:37:13 +0200

    added game state stuff

--- creep.js
+++ creep.js
@@ -1,3 +1,20 @@
+/**
+	A class in the canvas tower defense game!
+    Copyright (C) 2013  René van der Ark
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 var Creep = function(sprite, ctx, grid, options) {
 	var opts = options || {x: 0, y: 0};
 	var dimz = grid.getRealPos({x: opts.x, y: opts.y});
